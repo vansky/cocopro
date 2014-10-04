9e87c3ef16cab3f520ef29d3dac7508a8c781d1c
Marten van Schijndel <vanschm@ling.osu.edu>
2014-10-04 13:59:27 -0400

    cocopro: fixed several sentence segmentation issues;

--- scripts/segment_sentences.py
+++ scripts/segment_sentences.py
@@ -21,10 +21,10 @@ for aix in range(1,len(sys.argv)):
     OPTS[sys.argv[aix][2:]] = sys.argv[aix+1]
   
 
-reperiodendword = re.compile('[A-Za-z]\.[\'"]*') #find word-final periods
+reperiodendword = re.compile('[A-Za-z0-9]\.[\'"`]*') #find word-final periods
 reperiodinword = re.compile('[A-Za-z]\.[A-Za-z]') #find word-internal punc (e.g. acronyms)
-repuncendword = re.compile('[A-Za-z][\?!][\'"]*') #find word-final punc
-recap = re.compile('[A-Z]') #find caps
+repuncendword = re.compile('[A-Za-z0-9][\?!][\'"`]*') #find word-final punc
+recap = re.compile('[\'"`]*[A-Z]') #find caps
 
 exceptions = ['mr','mrs','ms','eg','ie','etc'] #these and all words with word-internal periods
 
@@ -42,19 +42,28 @@ overix = 0 #ctr to track overall position in text
 sentbounds = [0]
 
 NUMLINES = len(text)
+FIRST = True
 for lix,line in enumerate(text):
   sline = line.strip().split()
   if sline == []:
-    sentbounds.append(overix)
+    if not FIRST:
+      sentbounds.append(overix)
+    continue
+  FIRST = False
     
   for wix,word in enumerate(sline):
     #if there's non-period punctuation at the end of word (and we're not at the end of the file) OR
-      #the word ends with a period AND the word isn't an exception AND the word begins with a capital AND the word has no internal periods
+      #the word ends with a period AND the word isn't an exception AND the next word begins with a capital AND the word has no internal periods
       # we assume sentences can't end with acronyms, which isn't true, but it's what we're assuming for now
-    if (repuncendword.search(word) and (lix < NUMLINES or wix < len(sline) )) or \
-          ( reperiodendword.search(word) and word not in exceptions and wix < len(sline)-1 and recap.match(sline[wix+1]) and not reperiodinword.search(word) ):
+    if (repuncendword.search(word) and (lix < NUMLINES - 1 or wix < len(sline) )) or \
+          ( reperiodendword.search(word) and ''.join(word.lower().split('.')) not in exceptions and \
+            (( wix < len(sline)-1 and recap.match(sline[wix+1])) or \
+             (lix < NUMLINES - 1 and text[lix+1].strip().split() != [] and recap.match(text[lix+1].strip().split()[0]))) and not \
+              reperiodinword.search(word)):
       sentbounds.append(overix + wix + 1) #if this is the end of a sentence, the NEXT ix is the beginning of a sentence
   overix += len(sline)
 
-with open(OPTS['output'],'wb') as f:
-  pickle.dump(sentbounds,f)
+with open(OPTS['output'],'w') as f:
+  #pickle.dump(sentbounds,f)
+  for s in sentbounds:
+    f.write(str(s)+'\n')
