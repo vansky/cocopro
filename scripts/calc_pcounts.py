464ae740127f6224edfc3a88535bab1c12c0f993
Marten van Schijndel <vanschm@ling.osu.edu>
2014-10-10 19:26:39 -0400

    cocopro: pseudocounts now added to pro_from_x dicts;

--- scripts/calc_pcounts.py
+++ scripts/calc_pcounts.py
@@ -228,15 +228,15 @@ for e in coco_corpus:
 
 #Possible values for PRO:
 #  10: ['PRE', 'NOM', 'BAR', 'PRO', 'HLS', 'NAM', 'ARC', 'WHQ', 'PTV', 'APP']
-posspros = ['PRE', 'NOM', 'BAR', 'PRO', 'HLS', 'NAM', 'ARC', 'WHQ', 'PTV', 'APP']
-prior = 1 / len(posspros)
-for d in [pro_from_ref, pro_from_coh, pro_from_top, pro_from_sent]:
-  #assign uniform priors over the set of possible PRO values
-  for outer in d:
-    for pro in posspros:
-      d[outer][pro] = d[outer].get(pro,0) + prior
+#posspros = ['PRE', 'NOM', 'BAR', 'PRO', 'HLS', 'NAM', 'ARC', 'WHQ', 'PTV', 'APP']
+#prior = 1 / len(posspros)
+#for d in [pro_from_ref, pro_from_coh, pro_from_top, pro_from_sent]:
+#  #assign uniform priors over the set of possible PRO values
+#  for outer in d:
+#    for pro in posspros:
+#      d[outer][pro] = d[outer].get(pro,0) + prior
 
-for d in (ref_from_coh, ref_from_top, pro_from_coh):
+for d in (pro_from_ref, pro_from_coh, pro_from_top, pro_from_sent, ref_from_coh, ref_from_top):
   d = add_pseudocounts(d)
 #Possible values for COH:
 #  127: ['elab-det-time-org-num', 'elab-num-pers-det', 'elab-loc', 'elab-det-num-pers-org', 'elab-det-loc-org-time', 'elab-loc-pers', 'elab', 'elab-time-pers-loc', 'elab-det-org-num-time', 'elab-pers-time-loc', 'elab-det-pers', 'elab-det-time-org', 'elab-pers-det', 'elab-pers-org-det', 'elab-pers-det-org-time-num', 'elab-num-time', 'elab-det-num-time', 'elab-det-time-pers', 'elab-det-loc-num-org', 'elab-det-num-pers-org-loc-time', 'elab-pers-loc', 'gen', 'elab-pers-det-time-num', 'attr', 'elab-time', 'elab-time-num', 'elab-det-num-loc-time', '-1', 'elab-det-pers-time-org', 'elab-pers-org', 'elab-det-time-org-pers', 'elab-org-pers-det', 'elab-loc-det', 'elab-pers-num', 'elab-det-org-pers', 'elab-det-num', 'elab-det-time-loc', 'elab-time-det-num', 'elab-det-num-pers', 'elab-pers-time', 'elab-det-pers-time', 'elab-det-pers-org-time-num', 'elab-det-pers-loc-org', 'contrast', 'elab-num-pers', 'elab-det-org-loc', 'elab-org', 'elab-det-num-loc', 'elab-det-num-pers-time', 'elab-org-num', 'elab-det-pers-loc', 'elab-det-org-num', 'elab-det-pers-org-time', 'elab-org-time', 'elab-pers-loc-num', 'elab-loc-org-num', 'elab-det-time-num-org-loc-pers', 'elab-pers-loc-time', 'contr', 'elab-time-org', 'elab-det', 'elab-det-pers-org-num', 'elab-loc-time-det', 'elab-det-org-time-num-pers', 'elab-det-pers-time-loc', 'ce', 'elab-pers-det-loc', 'elab-det-pers-num-org', 'elab-dec-loc-pers', 'elab-det-num-org-loc', 'elab-per', 'elab-det-time', 'elab-det-pers-org', 'elab-detg', 'elab-org-det', 'elab-det-loc-num', 'elab-det-pers-org-loc', 'elab-time-loc', 'elab-det-loc-org-num', 'elab-time-num-det', 'elab-org-pers', 'elab-time-det-loc', 'elab-det-loc-org', 'elab-num-time-det', 'elab-det-pers-org-num-loc', 'elab-num-loc-org-pers', 'elab-pers-time-org', 'elab-loc-org', 'elab-det-org-loc-pers', 'temp', 'expv', 'elab-det-time-num-org', 'elab-det-pers-loc-org-num-time', 'elab-det-pers-loc-time-org', 'elab-det-loc-pers-time', 'elab-time-det', 'elab-pers-org-loc', 'elab-det-time-loc-org', 'elab-loc-det-pers', 'elab-num', 'cond', 'elab-det-pers-num', 'elab-num-loc-det', 'elab-time-det-org-pers', 'elab-pers-det-time', 'par', 'elab-loc-org-pers-det', 'same', 'elab-det-pers-time-num', 'examp', 'elab-det-loc-time', 'elab-det-loc', 'elab-det-loc-pers', 'elab-num-org-time', 'elab-pers', 'elab-det-org', 'elab-det-org-time-loc-num-pers', 'elab-det-pers-loc-time', 'elab-det-num-org', 'parallel', 'elab-num-loc-pers-det', 'elab-det-time-pers-num', 'elab-det-num-time-loc', 'elab-num-det-pers', 'elab-det-num-pers-loc', 'elab-num-loc-det-pers', 'elab-det-time-num']
