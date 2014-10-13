982d34a47449c30f9677546e070b93b7f365e185
Marten van Schijndel <vanschm@ling.osu.edu>
2014-10-12 23:52:39 -0400

    cocopro: added pseudocount prior distortions at the document level;

--- scripts/calc_pcounts.py
+++ scripts/calc_pcounts.py
@@ -12,6 +12,7 @@ import re
 import sys
 
 VERBOSE=False #Exposes a couple bugs we'll likely need to address later as they make the data sloppy (might be inherent in c3)
+WEAKPRIOR=True #Weakens/Strengthens prior PRO expectations
 
 OPTS = {}
 for aix in range(1,len(sys.argv)):
@@ -28,23 +29,38 @@ def get_topic(line):
   #returns the topic assignment from this line
   return(line.strip().split()[-1])
 
-def add_pseudocounts(indict):
+def add_pseudocounts(indict,priordict=None):
   if len(indict) == 0:
     return(indict)
   #adds 1 pseudo observation to all conditions (divided among marginals+unk)
   if type(indict[list(indict)[0]]) == type({}):
     #conditional probability dict
     for k in indict:
-      indict[k] = add_pseudocounts(indict[k])
-    #add one pseudo count for unseen conditions that can split to all observed marginals
-    poss_keys = set([m for k in indict for m in indict[k]])
-    indict['-1'] = dict([(k, 1/len(poss_keys)) for k in poss_keys])
+      indict[k] = add_pseudocounts(indict[k],priordict)
+    if not priordict:
+      #add one pseudo count for unseen conditions that can split to all observed marginals
+      poss_keys = set([m for k in indict for m in indict[k]])
+      indict['-1'] = dict([(k, 1/len(poss_keys)) for k in poss_keys])
+    else:
+      #add pseudo counts for unseen conditions that can split based on prior expectations
+      poss_keys = len(priordict) + 1
+      indict['-1'] = priordict
+      for k in priordict:
+        indict['-1'][k] += 1/poss_keys
+      indict['-1']['-1'] = 1/poss_keys
   else:
-    #marginal probability dist
-    numkeys = len(indict)+1
-    for k in indict:
-      indict[k] += 1/numkeys
-    indict['-1'] = 1/numkeys
+    if not priordict:
+      #marginal probability dist
+      numkeys = len(indict)+1
+      for k in indict:
+        indict[k] += 1/numkeys
+      indict['-1'] = 1/numkeys
+    else:
+      #include prior expectation pseudo counts
+      poss_keys = len(priordict) + 1
+      for k in priordict:
+        indict[k] = indict.get(k,0)+priordict[k] + 1/poss_keys
+      indict['-1'] = 1/poss_keys
   return(indict)
 
 with open(OPTS['coco-corpus'], 'rb') as f:
@@ -63,6 +79,7 @@ with open(OPTS['sentences'], 'r') as f:
 PRONOUNS = ['he','she','they','we','I','you','them','that','those','it','one']
 #sent_counts = {'-1': {}} # { [word] : { [topic] : [counts] } }
 coh_counts = {} # { [coh] : [counts] }
+pro_counts = {} # { [pro] : [counts] }
 
 #binary
 pro_from_ref = {} # { [ref] : { [pro] : [counts] } }
@@ -156,6 +173,8 @@ for e in coco_corpus:
   #binary pro
 #  pro = str(topics[head_begin].split()[0].lower() in PRONOUNS) #possible: 'True', 'False'
   pro = e['TYPE'] #mention type
+  pro_counts[pro] = pro_counts.get(pro,0) + 1
+  
   if ref not in pro_from_ref:
     pro_from_ref[ref] = {}
   pro_from_ref[ref][pro] = pro_from_ref[ref].get(pro,0) + 1
@@ -246,7 +265,22 @@ for e in coco_corpus:
 #    for pro in posspros:
 #      d[outer][pro] = d[outer].get(pro,0) + prior
 
-for d in (pro_from_ref, pro_from_coh, pro_from_top, pro_from_sent, pro_from_ant, ref_from_coh, ref_from_top):
+if pro_counts != {}:
+  if WEAKPRIOR:
+    #normalize to largest thing to weaken prior counts
+    bigkey = max(pro_counts.values())
+    for p in pro_counts:
+      pro_counts[p] = pro_counts[p] / bigkey
+  else:
+    #normalize to smallest thing to strengthen prior counts
+    smallkey = min(pro_counts.values())
+    for p in pro_counts:
+      pro_counts[p] = pro_counts[p] / smallkey
+    
+for d in (pro_from_ref, pro_from_coh, pro_from_top, pro_from_sent, pro_from_ant):
+  d = add_pseudocounts(d,pro_counts)
+
+for d in (ref_from_coh, ref_from_top):
   d = add_pseudocounts(d)
 #Possible values for COH:
 #  127: ['elab-det-time-org-num', 'elab-num-pers-det', 'elab-loc', 'elab-det-num-pers-org', 'elab-det-loc-org-time', 'elab-loc-pers', 'elab', 'elab-time-pers-loc', 'elab-det-org-num-time', 'elab-pers-time-loc', 'elab-det-pers', 'elab-det-time-org', 'elab-pers-det', 'elab-pers-org-det', 'elab-pers-det-org-time-num', 'elab-num-time', 'elab-det-num-time', 'elab-det-time-pers', 'elab-det-loc-num-org', 'elab-det-num-pers-org-loc-time', 'elab-pers-loc', 'gen', 'elab-pers-det-time-num', 'attr', 'elab-time', 'elab-time-num', 'elab-det-num-loc-time', '-1', 'elab-det-pers-time-org', 'elab-pers-org', 'elab-det-time-org-pers', 'elab-org-pers-det', 'elab-loc-det', 'elab-pers-num', 'elab-det-org-pers', 'elab-det-num', 'elab-det-time-loc', 'elab-time-det-num', 'elab-det-num-pers', 'elab-pers-time', 'elab-det-pers-time', 'elab-det-pers-org-time-num', 'elab-det-pers-loc-org', 'contrast', 'elab-num-pers', 'elab-det-org-loc', 'elab-org', 'elab-det-num-loc', 'elab-det-num-pers-time', 'elab-org-num', 'elab-det-pers-loc', 'elab-det-org-num', 'elab-det-pers-org-time', 'elab-org-time', 'elab-pers-loc-num', 'elab-loc-org-num', 'elab-det-time-num-org-loc-pers', 'elab-pers-loc-time', 'contr', 'elab-time-org', 'elab-det', 'elab-det-pers-org-num', 'elab-loc-time-det', 'elab-det-org-time-num-pers', 'elab-det-pers-time-loc', 'ce', 'elab-pers-det-loc', 'elab-det-pers-num-org', 'elab-dec-loc-pers', 'elab-det-num-org-loc', 'elab-per', 'elab-det-time', 'elab-det-pers-org', 'elab-detg', 'elab-org-det', 'elab-det-loc-num', 'elab-det-pers-org-loc', 'elab-time-loc', 'elab-det-loc-org-num', 'elab-time-num-det', 'elab-org-pers', 'elab-time-det-loc', 'elab-det-loc-org', 'elab-num-time-det', 'elab-det-pers-org-num-loc', 'elab-num-loc-org-pers', 'elab-pers-time-org', 'elab-loc-org', 'elab-det-org-loc-pers', 'temp', 'expv', 'elab-det-time-num-org', 'elab-det-pers-loc-org-num-time', 'elab-det-pers-loc-time-org', 'elab-det-loc-pers-time', 'elab-time-det', 'elab-pers-org-loc', 'elab-det-time-loc-org', 'elab-loc-det-pers', 'elab-num', 'cond', 'elab-det-pers-num', 'elab-num-loc-det', 'elab-time-det-org-pers', 'elab-pers-det-time', 'par', 'elab-loc-org-pers-det', 'same', 'elab-det-pers-time-num', 'examp', 'elab-det-loc-time', 'elab-det-loc', 'elab-det-loc-pers', 'elab-num-org-time', 'elab-pers', 'elab-det-org', 'elab-det-org-time-loc-num-pers', 'elab-det-pers-loc-time', 'elab-det-num-org', 'parallel', 'elab-num-loc-pers-det', 'elab-det-time-pers-num', 'elab-det-num-time-loc', 'elab-num-det-pers', 'elab-det-num-pers-loc', 'elab-num-loc-det-pers', 'elab-det-time-num']
