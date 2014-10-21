b26c71ea4ce31387e08b52030fe3ed84368f2b4d
Marten van Schijndel <vanschm@ling.osu.edu>
2014-10-21 13:44:26 -0400

    cocopro: cleaned up weight regression script;

--- scripts/find_feature_weights.py
+++ scripts/find_feature_weights.py
@@ -9,8 +9,8 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn import svm
 import sys
 
-DEP_VAL = "pro"
-OMIT_VALS = ["ref_id"]
+DEP_VAL = "pro" #dependent variable to regress to when determining weights
+OMIT_VALS = ["ref_id"] #features to omit when determining feature weights
 RANDOM_SEED = 37 #None yields random initialization
 
 #OPTS = {}
@@ -29,19 +29,18 @@ for infile in sys.argv[1:]:
   with open(infile,'rb') as f:
     indict += pickle.load(f)
 
+#since all categorial values are one hot, each vector associated w/ a feature is FEATURE=X, so ensure the entire feature name should be omitted
 DEP_VAL = DEP_VAL + '='
 for i,v in enumerate(OMIT_VALS):
   OMIT_VALS[i] = OMIT_VALS[i]+'='
 
 #suck the input dict lists in, encode categorial variables as one-hot, and store as a scipy.sparse matrix of [obs][feature_vec_element]
 myobs = DictVectorizer()
-#myobs.fit_transform(indict)
 #myobs_scaled = sklearn.preprocessing.scale(myobs, with_mean=False) #memory-efficient, scales (but doesn't center) all features/columns
 myobs_a = myobs.fit_transform(indict).toarray()
 myobs_scaled = sklearn.preprocessing.scale(myobs_a) #less memory efficient, but centers and scales all features/columns
 
 #Regress an SVM classifier to distinguish between the classes and output the weights
-#sys.stderr.write(str(myobs.get_feature_names())+'\n')
 dep_ix = []
 omit_ix = []
 feature_names = myobs.get_feature_names()
@@ -59,16 +58,11 @@ if dep_ix == []:
 y_raw = myobs_a[:,dep_ix[0]].reshape(-1,1) #pull out the dependent var columns
 for i in dep_ix[1:]:
   y_raw = numpy.append(y_raw,myobs_a[:,i].reshape(-1,1),1)  #Need to append columns to one another!
-  #sys.stderr.write(str(y_raw.shape)+'\n')
 y = numpy.nonzero(y_raw)[1].astype("float") #grab the dimension that's one-hot for the dependent variable
-#y = [float(i) for i in y]
-#sys.stderr.write(str(y_raw[:100,:])+'\n')
-#sys.stderr.write(str(y[:100])+'\n')
-y_scaled = sklearn.preprocessing.scale(y.reshape(-1,1)).ravel()
+y_scaled = sklearn.preprocessing.scale(y.reshape(-1,1)).ravel() #scale and center the dependent variable (then convert to a flattened array
 X = myobs_scaled[:,:omit_ix[0]]
 for i in range(len(omit_ix[1:-1])):
   X = numpy.append(X,myobs_scaled[:,omit_ix[i-1]:omit_ix[i]],1) #append any intervening columns... might never happen due to OneHot
-#X = numpy.delete(myobs_scaled,omit_ix,1) #created a matrix of independent vars that doesn't include the dependent var
 X = numpy.append(X,myobs_scaled[:,omit_ix[-1]:],1) #append the rest of myobs_scaled
 
 #sys.stderr.write(str(y_scaled.shape)+'~'+str(X.shape)+'\n')
@@ -91,11 +85,6 @@ for fix in range(len(feature_names)):
     #need to skip the (now-missing) omitted variable columns
     fixmod -= 1
     continue
-    #sys.stdout.write(str(feature_names[fix+1])+': '+str(svm_classifier.coef_[0][fix])+'\n')
-  else:
-    pass
-    #sys.stdout.write(str(feature_names[fix])+': '+str(svm_classifier.coef_[0][fix])+'\n')
-  #sys.stderr.write(str(feature_names[fix])+'\n')
   clean_name = feature_names[fix].split('=')[0]
   clean_coefs[clean_name] = clean_coefs.get(clean_name, 0) + svm_classifier.coef_[0][fix+fixmod]**2 #find the length of the categorial coef vector
 total = sum(math.sqrt(c) for c in clean_coefs.values())
